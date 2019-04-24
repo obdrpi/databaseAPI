11cb1acfd08a6988026c732ee09fa92d0b8572b3
SARTHAK JAIN <sjain413@gatech.edu>
2019-04-24 13:39:24 -0400

    added

--- test_1.py
+++ test_1.py
@@ -27,6 +27,12 @@ class TestAPI(unittest.TestCase):
         result = readLog(timestamp1, timestamp2)
         self.assertEqual(result, 200)
 
+    def test_json_log4(self):
+        string = '{"log":{"remote_ip": "93.180.71.3", "remote_user": "-", "request": "GET /downloads/product_1 HTTP/1.1", "response": 304, "bytes": 0, "referrer": "-", "agent": "Debian APT-HTTP/1.3 (0.8.16~exp12ubuntu10.21)"}}'
+        payload = json.loads(string)
+        result = insertLog(payload)
+        self.assertEqual(result, 200)
+
 if __name__ == "__main__":
     unittest.main()
     print "Everything Passed"
